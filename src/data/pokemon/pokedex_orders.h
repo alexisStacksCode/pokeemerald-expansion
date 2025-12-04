345eaf2645b88fd9539c462df75e7ae528d5be7a
alexisStacksCode <91137432+alexisStacksCode@users.noreply.github.com>
2025-12-04 11:30:47 +0600

    Update pokedex_orders.h

--- src/data/pokemon/pokedex_orders.h
+++ src/data/pokemon/pokedex_orders.h
@@ -1,24 +1,12 @@
 // Only list base species here.
 const u16 gPokedexOrder_Alphabetical[] =
 {
-    NATIONAL_DEX_DAPHNE_BLAKE,
-    NATIONAL_DEX_HORUS,
-    NATIONAL_DEX_KHOPESH,
-    NATIONAL_DEX_SCARAB,
 };
 
 const u16 gPokedexOrder_Weight[] =
 {
-    NATIONAL_DEX_DAPHNE_BLAKE,
-    NATIONAL_DEX_HORUS,
-    NATIONAL_DEX_KHOPESH,
-    NATIONAL_DEX_SCARAB,
 };
 
 const u16 gPokedexOrder_Height[] =
 {
-    NATIONAL_DEX_DAPHNE_BLAKE,
-    NATIONAL_DEX_HORUS,
-    NATIONAL_DEX_KHOPESH,
-    NATIONAL_DEX_SCARAB,
 };
