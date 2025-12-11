3e00e897d993bb09bc4eaa62b4454df0cc6cbdf9
alexisStacksCode <91137432+alexisStacksCode@users.noreply.github.com>
2025-12-11 16:41:52 +0600

    Create run_mid2agb.py

---
+++ run_mid2agb.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+import shutil
+
+
+MID2AGB_DIR: str = "tools/mid2agb"
+MUSIC_DIR: str = "sound/songs/midi"
+
+
+music_files_to_process: list[str] = [file for file in os.listdir("tools/mid2agb") if os.path.splitext(file)[1] == ".mid"]
+
+
+if os.name == "posix":
+    for music_file in music_files_to_process:
+        # Convert source MIDI to GBA AGB.
+        subprocess.run([f"{MID2AGB_DIR}/mid2agb", f"{MID2AGB_DIR}/{music_file}", f"{MUSIC_DIR}/{music_file.replace(".mid", ".s")}", "-V50", "-G191"])
+
+        # Copy source MIDI.
+        shutil.copy2(f"{MID2AGB_DIR}/{music_file}", f"{MUSIC_DIR}/{music_file}")
+else:
+    print(f"'{__file__}' will not work on non-Linux systems, exiting...")
