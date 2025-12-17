cfdb946ac0e8e72947c2ce7fef18a16d31bf57b5
alexisStacksCode <91137432+alexisStacksCode@users.noreply.github.com>
2025-12-17 12:33:19 +0600

    Remove Badge Boost mechanic

--- src/battle_util.c
+++ src/battle_util.c
@@ -7599,24 +7599,6 @@ static bool32 IsRuinStatusActive(u32 fieldEffect)
     return FALSE;
 }
 
-static inline uq4_12_t ApplyOffensiveBadgeBoost(uq4_12_t modifier, u32 battler, u32 move)
-{
-    if (ShouldGetStatBadgeBoost(B_FLAG_BADGE_BOOST_ATTACK, battler) && IsBattleMovePhysical(move))
-        modifier = uq4_12_multiply_half_down(modifier, GetBadgeBoostModifier());
-    if (ShouldGetStatBadgeBoost(B_FLAG_BADGE_BOOST_SPATK, battler) && IsBattleMoveSpecial(move))
-        modifier = uq4_12_multiply_half_down(modifier, GetBadgeBoostModifier());
-    return modifier;
-}
-
-static inline uq4_12_t ApplyDefensiveBadgeBoost(uq4_12_t modifier, u32 battler, u32 move)
-{
-    if (ShouldGetStatBadgeBoost(B_FLAG_BADGE_BOOST_DEFENSE, battler) && IsBattleMovePhysical(move))
-        modifier = uq4_12_multiply_half_down(modifier, GetBadgeBoostModifier());
-    if (ShouldGetStatBadgeBoost(B_FLAG_BADGE_BOOST_SPDEF, battler) && IsBattleMoveSpecial(move))
-        modifier = uq4_12_multiply_half_down(modifier, GetBadgeBoostModifier());
-    return modifier;
-}
-
 static inline u32 CalcAttackStat(struct DamageContext *ctx)
 {
     u8 atkStage;
@@ -7689,7 +7671,7 @@ static inline u32 CalcAttackStat(struct DamageContext *ctx)
     modifier = UQ_4_12(1.0);
 
     if (ctx->isSelfInflicted)
-        return uq4_12_multiply_by_int_half_down(ApplyOffensiveBadgeBoost(modifier, battlerAtk, move), atkStat);
+        return uq4_12_multiply_by_int_half_down(modifier, atkStat);
 
     // attacker's abilities
     switch (ctx->abilityAtk)
@@ -7891,8 +7873,6 @@ static inline u32 CalcAttackStat(struct DamageContext *ctx)
         break;
     }
 
-    modifier = ApplyOffensiveBadgeBoost(modifier, battlerAtk, move);
-
     return uq4_12_multiply_by_int_half_down(modifier, atkStat);
 }
 
@@ -7977,7 +7957,7 @@ static inline u32 CalcDefenseStat(struct DamageContext *ctx)
     modifier = UQ_4_12(1.0);
 
     if (ctx->isSelfInflicted)
-        return uq4_12_multiply_by_int_half_down(ApplyDefensiveBadgeBoost(modifier, battlerDef, move), defStat);
+        return uq4_12_multiply_by_int_half_down(modifier, defStat);
 
     // target's abilities
     switch (ctx->abilityDef)
@@ -8071,8 +8051,6 @@ static inline u32 CalcDefenseStat(struct DamageContext *ctx)
     // snow def boost for ice types
     if (IS_BATTLER_OF_TYPE(battlerDef, TYPE_ICE) && IsBattlerWeatherAffected(battlerDef, B_WEATHER_SNOW) && usesDefStat)
         modifier = uq4_12_multiply_half_down(modifier, UQ_4_12(1.5));
-
-    modifier = ApplyDefensiveBadgeBoost(modifier, battlerDef, move);
 
     return uq4_12_multiply_by_int_half_down(modifier, defStat);
 }
@@ -9603,30 +9581,6 @@ u32 TryImmunityAbilityHealStatus(u32 battler, enum AbilityEffect caseID)
     }
 
     return 0;
-}
-
-uq4_12_t GetBadgeBoostModifier(void)
-{
-    if (GetConfig(CONFIG_BADGE_BOOST) < GEN_3)
-        return UQ_4_12(1.125);
-    else
-        return UQ_4_12(1.1);
-}
-
-bool32 ShouldGetStatBadgeBoost(u16 badgeFlag, u32 battler)
-{
-    if (GetConfig(CONFIG_BADGE_BOOST) <= GEN_3 && badgeFlag != 0)
-    {
-        if (gBattleTypeFlags & (BATTLE_TYPE_LINK | BATTLE_TYPE_EREADER_TRAINER | BATTLE_TYPE_RECORDED_LINK | BATTLE_TYPE_FRONTIER))
-            return FALSE;
-        else if (!IsOnPlayerSide(battler))
-            return FALSE;
-        else if (gBattleTypeFlags & BATTLE_TYPE_TRAINER && TRAINER_BATTLE_PARAM.opponentA == TRAINER_SECRET_BASE)
-            return FALSE;
-        else if (FlagGet(badgeFlag))
-            return TRUE;
-    }
-    return FALSE;
 }
 
 static enum DamageCategory SwapMoveDamageCategory(u32 move)
